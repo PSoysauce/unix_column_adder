19dfbbfd6c9e1cb7b2dd6200e405159dfec7cf33
PSoysauce <PSoysauce@gmail.com>
2019-05-15 23:11:22 -0500

    Added head.h which will be the header file that will contain the map used to hold column number and file name

---
+++ head.h
@@ -0,0 +1,22 @@
+/*  Name - Patrick Soisson
+ *  Email - patrick.b.soisson@gmail.com
+ *  Date - 5/15/19
+ *
+ *  Description head.h is used to connect main.cc and tclap.cc so they both can use the enums in the global map variable and to also 
+ *  have access to functions in eachother
+ */
+
+  #include <map>
+  #include <string>
+  #include <iostream>
+  using namespace std;
+
+  #ifndef head_h 
+  #define head_h
+
+  extern map<int, string> m;
+  enum par{column_number, file_name};
+  bool tclap(int, char**);
+
+  #endif
+
