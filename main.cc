5563d39d16a06d2f7551e9805edb37eeaa10c9a8
PSoysauce <PSoysauce@gmail.com>
2019-05-15 23:36:57 -0500

    Added main.cc which will be used to call tclap and all the other functions needed

---
+++ main.cc
@@ -0,0 +1,18 @@
+/* Name - Patrick Soisson
+ * Email - patrick.b.soisson@gmail.com
+ * Date - 5/15/19
+ *
+ * Description - main.cc will hold all the main functionality of this program
+ *
+ */
+ 
+
+ #include <iostream>
+ #include "head.h"
+ using namespace std;
+
+ int main(int argc, char** argv) {
+    cout << "Hello" << endl;
+    tclap(argc, argv);
+    return 0;
+ }
