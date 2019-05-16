c4c2f12411a113c6a0614a87a879746f2c688bc8
PSoysauce <PSoysauce@gmail.com>
2019-05-16 00:11:42 -0500

    Added call to column_addition in main

--- main.cc
+++ main.cc
@@ -6,13 +6,11 @@
  *
  */
  
-
- #include <iostream>
  #include "head.h"
- using namespace std;
 
  int main(int argc, char** argv) {
-    cout << "Hello" << endl;
     tclap(argc, argv);
+    int sum = column_addition();
+    cout << sum << endl;
     return 0;
  }
